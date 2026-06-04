6880adc5f9128ffe3bb766c6dd27ea7b35847f3c
MohammedAsTech <mmm579aiman@gmail.com>
2026-06-04 14:57:09 +0300

    finishing H file for persistence

--- include/Persistence.h
+++ include/Persistence.h
@@ -1,8 +1,20 @@
-//
-// Created by moham on 04/06/2026.
-//
-
 #ifndef PERSISTENCE_H
 #define PERSISTENCE_H
 
-#endif //PERSISTENCE_H
+#include <string>
+
+class Database;
+
+class Persistence {
+public:
+    Persistence();
+    ~Persistence();
+
+    void saveToFile(const Database& db,
+                    const std::string& filename) const;
+
+    void loadFromFile(Database& db,
+                      const std::string& filename) const;
+};
+
+#endif // PERSISTENCE_H
